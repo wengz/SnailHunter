e7969d788f9f6797076182198721ad86979244cf
翁志成 <zhicheng.weng@ucarinc.com>
2019-07-30 02:32:32 +0800

    111

--- SnailHunter/snailhunterrt/src/main/java/pers/wengzc/snailhunterrt/HunterReportHandler.java
+++ SnailHunter/snailhunterrt/src/main/java/pers/wengzc/snailhunterrt/HunterReportHandler.java
@@ -11,10 +11,28 @@ public class HunterReportHandler extends Handler {
 
     private static volatile HunterReportHandler sInstance;
 
+    private static volatile Object sMainThreadLock = new Object();
+
+    private static volatile HunterReportHandler sMainInstance;
+
     private static HandlerThread sHandlerThread = new HandlerThread("SnailHunterReportThread");
+
+    private static HandlerThread sMainHandlerThread = new HandlerThread("MainSnailHunterReportThread");
 
     private HunterReportHandler (Looper looper){
         super(looper);
+    }
+
+    static HunterReportHandler getMainInstance() {
+        if (sMainInstance == null){
+            synchronized (sMainThreadLock){
+                if (sMainInstance == null){
+                    sMainHandlerThread.start();
+                    sMainInstance = new HunterReportHandler(sMainHandlerThread.getLooper());
+                }
+            }
+        }
+        return sMainInstance;
     }
 
     static HunterReportHandler getInstance() {
--- SnailHunter/snailhunterrt/src/main/java/pers/wengzc/snailhunterrt/SnailHunter.java
+++ SnailHunter/snailhunterrt/src/main/java/pers/wengzc/snailhunterrt/SnailHunter.java
@@ -4,6 +4,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import pers.wengzc.hunterkit.MethodInfoHandler;
 import pers.wengzc.snailhunterrt.ISnailHunterService;
 import pers.wengzc.snailhunterrt.SnailHunterService;
@@ -19,17 +23,21 @@ public class SnailHunter {
 
     public static Context sContext;
 
-    private static  ConnectServiceFuture sConnectServiceFuture;
+    private static ConnectServiceFuture sConnectServiceFuture;
 
-    public static boolean isHunterProcess (Context context){
+    private static ExecutorService executorService;
+
+    public static boolean isHunterProcess(Context context) {
         String processName = ProcessUtils.myProcessName(context);
         return processName != null && processName.endsWith(":" + HUNTER_PROCESS_NAME);
     }
 
-    public static void install (Context context){
-        if (isHunterProcess(context)){
+    public static void install(Context context) {
+        if (isHunterProcess(context)) {
             return;
         }
+
+        executorService = Executors.newSingleThreadExecutor();
 
         sContext = context;
         connectServiceIfNot();
@@ -46,54 +54,56 @@ public class SnailHunter {
                                                 final boolean isMainThread,
                                                 final boolean mainThreadConstraint,
                                                 final long timeConstraint) {
-                HunterReportHandler.getInstance().post(new Runnable() {
+                //只检查主线程，非主线程运行状态不捕捉
+                if (mainThreadConstraint && !isMainThread) {
+                    return;
+                }
+
+                //时间约束
+                long costTime = finishTime - startTime;
+                if (costTime < timeConstraint * 1000000) {
+                    return;
+                }
+
+
+                executorService.submit(new Runnable() {
                     @Override
                     public void run() {
 
-                        //只检查主线程，非主线程运行状态不捕捉
-//                        if (mainThreadConstraint && !isMainThread ){
-//                            return;
-//                        }
-//
-//                        //时间约束
-//                        long costTime = finishTime - startTime;
-//                        if (costTime < timeConstraint * 1000000){
-//                            return;
-//                        }
-
-                        //Log.d("xxx", "尝试获取 ISnailHunterService");
-                        Log.d("xxx", "run: className="+className+" methodName="+methodName);
+                        Log.d("xxx", "尝试获取 ISnailHunterService");
                         ISnailHunterService snailHunterService = getServiceSyncMayNull();
-                        //Log.d("xxx", "获取到ISnailHunterService");
-                        if (snailHunterService != null){
-                            try{
-                                //Log.d("xxx", "远程服务调用 snailHunterService.catchNewSnail");
-                                Snail rtSnail = new Snail( processId,  threadId,  threadName,
-                                         packageName,
-                                         className,
-                                         methodName,
-                                         startTime,
-                                         finishTime,
-                                         isMainThread,
-                                         mainThreadConstraint,
-                                         timeConstraint);
+                        Log.d("xxx", "获取到ISnailHunterService");
+                        if (snailHunterService != null) {
+                            try {
+                                Snail rtSnail = new Snail(processId, threadId, threadName,
+                                        packageName,
+                                        className,
+                                        methodName,
+                                        startTime,
+                                        finishTime,
+                                        isMainThread,
+                                        mainThreadConstraint,
+                                        timeConstraint);
+                                Log.d("xxx", "开始远程调用 className=" + className + " methodName=" + methodName);
                                 snailHunterService.catchNewSnail(rtSnail);
-                            }catch (Exception e){
+                            } catch (Exception e) {
                                 e.printStackTrace();
                             }
                         }
                     }
                 });
+
+
             }
 
         };
 
     }
 
-    private static void connectServiceIfNot (){
-        if (sConnectServiceFuture == null){
+    private static void connectServiceIfNot() {
+        if (sConnectServiceFuture == null) {
             sConnectServiceFuture = new ConnectServiceFuture();
-            if (sContext != null){
+            if (sContext != null) {
                 Log.d("xxx", "开始 bindService");
                 Intent intent = new Intent(sContext, SnailHunterService.class);
                 sContext.bindService(intent, sConnectServiceFuture, Context.BIND_AUTO_CREATE);
@@ -101,13 +111,13 @@ public class SnailHunter {
         }
     }
 
-    private static ISnailHunterService getServiceSyncMayNull (){
-        if (sConnectServiceFuture == null){
+    private static ISnailHunterService getServiceSyncMayNull() {
+        if (sConnectServiceFuture == null) {
             return null;
-        }else{
-            try{
+        } else {
+            try {
                 return sConnectServiceFuture.get();
-            }catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
             return null;
